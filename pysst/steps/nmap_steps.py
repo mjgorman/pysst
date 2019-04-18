fc46c88b4d9d8416fd23ed77dc629d11438029c9
Michael Gorman <michael@michaeljgorman.com>
2019-04-18 13:57:15 -0600

    Use correct template tags

--- pysst/steps/nmap_steps.py
+++ pysst/steps/nmap_steps.py
@@ -10,7 +10,7 @@ def step_nmap_is_installed(context):
 def step_we_scan_port(context, port):
     context.port = port
 
-@then('nmap will show <desired>')
+@then('nmap will show {desired}')
 def step_nmap_will_show_desired(context, desired):
     context.scanner.scan(context.target, context.port)
     state = context.scanner[context.target]['tcp'][int(context.port)]['state']
